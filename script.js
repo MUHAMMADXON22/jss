550813385a02dba89c5952188b0de409fbb394ac
MUKHAM <muxammadxon994@gmail.com>
2025-03-15 00:07:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('Oshibka')
+
+let name1 = prompt('String typedagi malumot kiriting')
+console.log(`  ${name1}`)
+
+let pul = prompt('Number typedagi malumot kiriting')
+console.log(`${pul},`)
+
+let pull = prompt('boolean typedagi malumot kiriting')
+console.log(`${pul},`)
